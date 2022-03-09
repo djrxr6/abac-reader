6e95304cbff77ec225bfb8d7ff1f08f7f16981ec
djrxr6 <danieljridd@gmail.com>
2022-03-09 23:07:10 +1100

    data writing to databse complete. Also implemented check against new adjudications

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import enum
 from os import sep
-from pathlib import Path
 from posixpath import split
+from xmlrpc.client import ResponseError
 from bs4 import BeautifulSoup
 
 import requests, logging, redis
-#import build_url_list as bul
-#import abac_reader as ar
 import pandas as pd
 
 def check_for_more_entries(li,i):
@@ -82,100 +79,69 @@ def get_urls_from_abac_page(page_url):
     for heading in list_of_headings:
         anchor_element = heading.a 
         url = anchor_element.get('href')
-        #urls_array.append(anchor_element.get('href'))
         urls_array.append(scrape_adjudication_page(url))
         #TODO: In here I can work out how to stop when the data is already recorded so as not to retrive it again.
     return urls_array
 
 def write_primary_results_csv_file(lines_array):
+
     output_file_name = "abac-adjudications-detailed.txt"
-    #Open a file (existing) to write output to. (will overwrite)
+
     with open(output_file_name, "w") as output_file:
-    #output_file = open("abac-adjudications-detailed.txt", "w")
         output_file.write("title;url;date;decision;brand;company;outcome;nature;medium;code_section\n")
-    #output_file.close()
 
     with open(output_file_name, "a") as output_file:
-    #output_file = open("abac-adjudications-detailed.txt", "a")
         for line in lines_array:
             output_file.write(line + "\n")
-    #output_file.close()
 
 def write_final_results_csv_file():
     results = pd.read_csv("abac-adjudications-detailed.txt",sep=";",index_col=False)
 
     print(results)
 
-
-    # new_column_names = [
-    # "M-Dig",
-    # "M-TV",
-    # "M-NP",
-    # "M-R",
-    # "M-C",
-    # "M-O",
-    # "M-POS",
-    # "M-P",
-    # "C-ai",
-    # "C-aii",
-    # "C-aiii",
-    # "C-aiv",
-    # "C-bi",
-    # "C-bii",
-    # "C-biii",
-    # "C-biv",
-    # "C-ci",
-    # "C-cii",
-    # "C-ciii",
-    # "C-civ",
-    # "C-d",
-    # "Month",
-    # "Year"
-    # ]
-
-    # for column_name in new_column_names:
-    #     results[column_name] = 0
-
-    results['M-Dig'] = results['medium'].apply(lambda row: 1 if ('Digital' in row) else 0)
-    results['M-TV'] = results['medium'].apply(lambda row: 1 if ('Television' in row) else 0)
-    results['M-NP'] = results['medium'].apply(lambda row: 1 if ('Naming/packaging' in row) else 0)
-    results['M-R'] = results['medium'].apply(lambda row: 1 if ('Radio' in row) else 0)
-    results['M-C'] = results['medium'].apply(lambda row: 1 if ('Cinema' in row) else 0)
-    results['M-O'] = results['medium'].apply(lambda row: 1 if ('Outdoor' in row) else 0)
-    results['M-POS'] = results['medium'].apply(lambda row: 1 if ('Point of sale' in row) else 0)
-    results['M-P'] = results['medium'].apply(lambda row: 1 if ('Print' in row) else 0)
-
-    results["C-ai"] = results['code_section'].apply(lambda row: 1 if ("(a)(i)" in row) else 0)
-    results["C-aii"] = results['code_section'].apply(lambda row: 1 if ("(a)(ii)" in row) else 0)
-    results["C-aiii"] = results['code_section'].apply(lambda row: 1 if ("(a)(iii)" in row) else 0)
-    results["C-aiv"] = results['code_section'].apply(lambda row: 1 if ("(a)(iv)" in row) else 0)
-    results["C-bi"] = results['code_section'].apply(lambda row: 1 if ("(b)(i)" in row) else 0)
-    results["C-bii"] = results['code_section'].apply(lambda row: 1 if ("(b)(ii)" in row) else 0)
-    results["C-biii"] = results['code_section'].apply(lambda row: 1 if ("(b)(iii)" in row) else 0)
-    results["C-biv"] = results['code_section'].apply(lambda row: 1 if ("(b)(iv)" in row) else 0)
-    results["C-ci"] = results['code_section'].apply(lambda row: 1 if ("(c)(i)" in row) else 0)
-    results["C-cii"] = results['code_section'].apply(lambda row: 1 if ("(c)(ii)" in row) else 0)
-    results["C-ciii"] = results['code_section'].apply(lambda row: 1 if ("(c)(iii)" in row) else 0)
-    results["C-civ"] = results['code_section'].apply(lambda row: 1 if ("(c)(iv)" in row) else 0)
-    results["C-d"] = results['code_section'].apply(lambda row: 1 if ("(d)" in row) else 0)
-
-    #print(results[["code_section",'C-bi','C-d','C-bii','C-ai']])
+    results = add_new_columns(results)
 
     print(results[["medium",'M-Dig','M-TV']])
     print(results.loc[0:3,'medium':'M-Dig'])
 
     results.to_csv('abac-adjudications-full-NEW.txt',sep=";",index=False)
 
+def add_new_columns(df):
+
+    df['M-Dig'] = df['medium'].apply(lambda row: 1 if ('Digital' in row) else 0)
+    df['M-TV'] = df['medium'].apply(lambda row: 1 if ('Television' in row) else 0)
+    df['M-NP'] = df['medium'].apply(lambda row: 1 if ('Naming/packaging' in row) else 0)
+    df['M-R'] = df['medium'].apply(lambda row: 1 if ('Radio' in row) else 0)
+    df['M-C'] = df['medium'].apply(lambda row: 1 if ('Cinema' in row) else 0)
+    df['M-O'] = df['medium'].apply(lambda row: 1 if ('Outdoor' in row) else 0)
+    df['M-POS'] = df['medium'].apply(lambda row: 1 if ('Point of sale' in row) else 0)
+    df['M-P'] = df['medium'].apply(lambda row: 1 if ('Print' in row) else 0)
+
+    df["C-ai"] = df['code_section'].apply(lambda row: 1 if ("(a)(i)" in row) else 0)
+    df["C-aii"] = df['code_section'].apply(lambda row: 1 if ("(a)(ii)" in row) else 0)
+    df["C-aiii"] = df['code_section'].apply(lambda row: 1 if ("(a)(iii)" in row) else 0)
+    df["C-aiv"] = df['code_section'].apply(lambda row: 1 if ("(a)(iv)" in row) else 0)
+    df["C-bi"] = df['code_section'].apply(lambda row: 1 if ("(b)(i)" in row) else 0)
+    df["C-bii"] = df['code_section'].apply(lambda row: 1 if ("(b)(ii)" in row) else 0)
+    df["C-biii"] = df['code_section'].apply(lambda row: 1 if ("(b)(iii)" in row) else 0)
+    df["C-biv"] = df['code_section'].apply(lambda row: 1 if ("(b)(iv)" in row) else 0)
+    df["C-ci"] = df['code_section'].apply(lambda row: 1 if ("(c)(i)" in row) else 0)
+    df["C-cii"] = df['code_section'].apply(lambda row: 1 if ("(c)(ii)" in row) else 0)
+    df["C-ciii"] = df['code_section'].apply(lambda row: 1 if ("(c)(iii)" in row) else 0)
+    df["C-civ"] = df['code_section'].apply(lambda row: 1 if ("(c)(iv)" in row) else 0)
+    df["C-d"] = df['code_section'].apply(lambda row: 1 if ("(d)" in row) else 0)
+
+    return df
+
 def get_final_page_index():
+
     logging.debug("CALL: get_final_page_index()")
     #take most recent highest index from database
     #increment until response 400, rewriting variable for each 200 response
     #update value in database
     #return final page number (first 400 response index, minus one)
     
-    #TODO: Retrieve final index from database
     final_page_number = DB_LAST_PAGE_INDEX
-
 
     logging.debug(f"Final page number from DB is: {final_page_number}")
 
@@ -191,19 +157,22 @@ def get_final_page_index():
         logging.debug(f'Final page URL response {final_page_response}')
         code = final_page_response.status_code
     
+    range_boundary = final_page_number
     final_page_number -= 1
 
     logging.debug("Loop exited")
     logging.debug(f"Final Page Number is: {final_page_number}")
+    logging.debug(f"Range boundary is: {range_boundary}")
 
-    #TODO: update database with new final index
     if DB_LAST_PAGE_INDEX == final_page_number:
         logging.debug('Last page index has not changed.')
     else:
         logging.debug('Last page index to be updated.')
+        db_data.set_last_page_index(final_page_number)
 
     logging.debug("EXIT: get_final_page_index()")
-    return final_page_number
+
+    return range_boundary
 
 def is_new_adjudications():
     logging.debug('CALL: is_new_adjudications()')
@@ -217,26 +186,42 @@ def is_new_adjudications():
 
     anchor_element = list_of_headings[0].a 
     url = anchor_element.get('href')
-    logging.debug('EXIT: is_new_adjudications()')
-    return url == get_most_recent_adjudication_in_db()
+    logging.debug(f'Most recent adjudication URL is {url}')
 
-def get_most_recent_adjudication_in_db():
-    return MOST_RECENT_ADJUDICATION_URL_FROM_DATABASE
+    db_url = db_data.get_most_recent_adjudication_url().strip('"')
+    logging.debug(f'Most recent adjudication URL in DB is {db_url}')
+
+    logging.debug('EXIT: is_new_adjudications()')
+    return url == db_url
+
+#def get_most_recent_adjudication_in_db():
+#    return MOST_RECENT_ADJUDICATION_URL_FROM_DATABASE
 
 class DbData:
     rd = ''
-    host, port ='192.168.1.117',6379
+    host, port = '192.168.1.117',6379
+
     def __init__(this):
         this.rd = redis.Redis(this.host, this.port, decode_responses=True)
 
     def get_last_page_index(this):
         return int(this.rd.get("LAST_PAGE_INDEX"))
+    
+    def set_last_page_index(this, val):
+        this.rd.set("LAST_PAGE_INDEX",val)
         
     def get_base_url(this):
         return this.rd.get("BASE_URL")
     
-    def get_most_recent_adjudication(this):
-        return this.rd.get("MOST_RECENT_ADJUDICATION")
+    def get_most_recent_adjudication_url(this):
+        try:
+            last_index = this.rd.execute_command('JSON.ARRLEN','abac_data','$')
+        except redis.exceptions.ResponseError:
+            url_at_index = ''
+        else:
+            last_record_position = last_index[0] -1
+            url_at_index = this.rd.execute_command('JSON.GET' ,'abac_data', f'[{last_record_position}].url')
+        return url_at_index
     
     def set_abac_data(this,json_string):
         this.rd.execute_command('JSON.SET','abac_data','$',json_string)
@@ -262,10 +247,13 @@ BASE_URL = db_data.get_base_url()
 
 DB_LAST_PAGE_INDEX = db_data.get_last_page_index()
 
-MOST_RECENT_ADJUDICATION_URL_FROM_DATABASE = db_data.get_most_recent_adjudication()
+json_existing_abac_data = db_data.get_abac_data()
 
-df_existing_abac_data = pd.read_json(db_data.get_abac_data())
-print(df_existing_abac_data)
+if json_existing_abac_data is None:
+    df_existing_abac_data = pd.DataFrame()
+else:
+    df_existing_abac_data = pd.read_json(db_data.get_abac_data())
+    #TODO: Need to look into how I can stop date conversion. Might be in the Redis class "decode"
 
 if not is_new_adjudications():
     
@@ -274,8 +262,7 @@ if not is_new_adjudications():
     abac_adjudications_pages_urls = []
     array_of_abac_data = []
 
-    for page_index in range(1, 2):
-    #for page_index in range(1, get_final_page_index()):
+    for page_index in range(1, get_final_page_index()):
         abac_adjudications_pages_urls.extend(get_urls_from_abac_page(f'{BASE_URL}{page_index}'))        
 
     columns = "title;url;date;decision;brand;company;outcome;nature;medium;code_section;temp_column"
@@ -283,36 +270,21 @@ if not is_new_adjudications():
 
     for line in abac_adjudications_pages_urls:
         array_of_abac_data.append(line.split(';'))
-        # df_abac_adjudication = pd.DataFrame([line.split(';')],columns=columns)
-        # json_abac_adjudication = df_abac_adjudication.to_json(orient="records")
-        # db_data.add_abac_data(json_abac_adjudication)
-
-    print(array_of_abac_data)
-
     
     df_new_abac_adjudications = pd.DataFrame(array_of_abac_data,columns=columns)
-    #TODO: remove temp column
-    print(df_new_abac_adjudications)
+    df_new_abac_adjudications = df_new_abac_adjudications.drop(columns=['temp_column'])
+    df_new_abac_adjudications = add_new_columns(df_new_abac_adjudications)
+    df_new_abac_adjudications = df_new_abac_adjudications.iloc[::-1]
 
-    df_updated_abac_adjudications = pd.concat([df_new_abac_adjudications,df_existing_abac_data],ignore_index=True)
+    df_updated_abac_adjudications = pd.concat([df_existing_abac_data,df_new_abac_adjudications],ignore_index=True)
 
     json_abac_adjudications = df_updated_abac_adjudications.to_json(orient="records")
-    print(json_abac_adjudications)
+
     db_data.set_abac_data(json_abac_adjudications)
 
+    #Currently keeping functionality to write csv file output
     #write_primary_results_csv_file(abac_adjudications_pages_urls)
-
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
-    #pd.set_option('display.max_colwidth', 10)
-
     #write_final_results_csv_file()
 
-    #print(abac_adjudications_pages_urls)
-
-    #for url in abac_adjudications_pages_urls:
-        #print(url)
-        #print(ar.scrape_adjudication_page(url))
 else:
     logging.debug("There are no new adjudications.")
