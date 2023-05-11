b4e76f6987316344298ebbe071a17e66d140bb61
djrxr6 <danieljridd@gmail.com>
2023-05-11 20:24:34 +1000

    continued reorg build

--- reorg_abac_data.py
+++ reorg_abac_data.py
@@ -5,8 +5,6 @@
 #       
 
 import pandas as pd
-import redis
-
 
 from modules.redis_connector import RedisConnector
 from modules.abac_data import AbacData
@@ -15,3 +13,27 @@ def main():
     redis_conector: RedisConnector = RedisConnector()
     abac_data: AbacData = AbacData(redis_conector) 
     data = abac_data.fetch_content_from_db()
+    print(data)
+    column_datatypes = {
+        'title': 'string',
+        'url': 'string',
+        'decision': 'string',
+        'brand': 'string',
+        'company': 'string',
+        'outcome': 'category',
+        'nature' : 'string',
+        'medium' : 'category'
+    }
+    df = pd.read_json(data, dtype=column_datatypes)
+    df['id'] = df.reset_index().index
+    df = df.astype({col: bool for col in df.columns[11:32]})
+    print(df.info())
+
+    print(pd.unique(df['outcome']))
+    print(pd.unique(df['medium']))
+
+    #Could store additional dataframes in redis
+    #Weekly/Monthly counts of medium and code secrions
+
+if __name__ == "__main__":
+    main()
